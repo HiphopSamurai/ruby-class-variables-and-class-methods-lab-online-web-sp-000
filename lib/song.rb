31f28f34c86ef52e5057a25c40a58d11e55f51a7
Garrett Thompson <gtcruz.gt@gmail.com>
2019-08-03 02:17:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,10 @@
 class Song
+  
+  attr_accessor :name, :artist, :genre
   @@count = 0
   @@genres = []
   @@artists = []
-  attr_accessor :name, :artist, :genre
-
+  
   def initialize
     @@count += 1
   end
